4b6d66862ccee362ae9dc25ea3ea0d4fb82c46cb
zyqidwiky123 <zyqidwiky@gmail.com>
2026-06-12 04:47:50 +0700

    chore: remove unused react-native driver app (aro-drive-driver-rn)

--- aro-drive-driver-rn/components/useColorScheme.ts
+++
@@ -1,6 +0,0 @@
-import { useColorScheme as useColorSchemeCore } from 'react-native';
-
-export const useColorScheme = () => {
-  const coreScheme = useColorSchemeCore();
-  return coreScheme === 'unspecified' ? 'light' : coreScheme;
-};
